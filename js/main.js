2ea4eddb16f7abef6900750b9a4b3ca95b70cf83
محمد الخلف <93102697+mohamad-khlaf@users.noreply.github.com>
2022-12-13 23:12:26 +0300

    ferst version

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+let mainImg = document.getElementById("main_img");
+
+let allSmallImg = document.querySelectorAll(".icons img");
+
+let container = document.querySelector(".container");
+
+
+let text = document.querySelector(".text");
+console.log(mainBtn)
+
+allSmallImg.forEach(function(img) {
+    
+    img.addEventListener("click", function() {
+
+        mainImg.src = img.getAttribute("src");
+        container.style.backgroundColor = this.getAttribute("data-bg-color");
+        mainBtn.style.color = this.getAttribute("data-bg-color");
+
+        allSmallImg.forEach(function(i) {
+            i.classList.remove("active");
+        })
+        this.classList.add('active');
+    })
+})
